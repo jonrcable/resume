ee8538d5f8ef41d7d57f26626d8842d0c9bf2cc3
spcdirect <jon@spcdirect.com>
2015-04-09 21:32:56 -0500

    test ignore

--- config/mail.php
+++ config/mail.php
@@ -76,7 +76,7 @@ return [
 	|
 	| If your SMTP server requires a username for authentication, you should
 	| set it here. This will get used to authenticate with your server on
-	| connection. You may also set the "password" value below this one.
+	| connection. You may also set the "password" value below this one. test
 	|
 	*/
 
